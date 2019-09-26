d918d9987d6c49c28a9a499348fd5104ecd15ea2
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2019-09-26 20:41:36 +0300

    version 1
    25 points

---
+++ svetoforysh.ino
@@ -0,0 +1,31 @@
+#define redLightPin 1
+#define yellowLightPin 2
+#define greenLightPin 3
+
+void setup (){
+
+pinMode (redLightPin, OUTPUT);
+pinMode (yellowLightPin, OUTPUT);
+pinMode (greenLightPin, OUTPUT);
+
+}
+
+void loop (){
+
+digitalWrite(redLightPin, HIGH);
+delay (1000);
+digitalWrite(redLightPin, LOW);
+
+digitalWrite(yellowLightPin, HIGH);
+delay (1000);
+digitalWrite(yellowLightPin, LOW);
+
+digitalWrite(greenLightPin, HIGH);
+delay (1000);
+digitalWrite(greenLightPin, LOW);
+
+digitalWrite(yellowLightPin, HIGH);
+delay (1000);
+digitalWrite(yellowLightPin, LOW);
+
+}
